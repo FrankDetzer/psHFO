42050e45cdbf8a8343868042c36de7207c56bc6f
Frankie D RSVLT <57404682+Frankie-D-RSVLT@users.noreply.github.com>
2020-11-07 21:57:01 +0100

    #

--- function/FormatTo-HumanFriendlyOutput/FormatTo-HumanFriendlyOutput.ps1
+++ function/FormatTo-HumanFriendlyOutput/FormatTo-HumanFriendlyOutput.ps1
@@ -1,6 +1,45 @@
 ﻿function FormatTo-HumanFriendlyOutput {
     param (
-        OptionalParameters
+        [parameter(ValueFromPipeline)]
+        [psobject]$InputObject
+        #     [parameter()]
+        #     [ValidateSet('Stack', 'Comma')]
+        #     [string]$OutputPropertyType = 'Stack'
+        #     [string]$Name,
+        #     [int]$Length
+        #     [switch]$DataMagnitude = 'byte'
     )
-    
+
+    begin {
+        $AllItems = @()
+    }
+    process {
+        $AllItems += [PSCustomObject][ordered]@{
+            'LengthInBytes'        = $InputObject.Length
+            'SizeInPercent'        = $null
+            'SizeInPercentRounded' = $null
+            'SizeVisualised'       = $null
+            'Name'                 = $InputObject.Name
+        }
+    }
+
+    end {
+        $TotalSize = ($AllItems | Measure-Object -Property LengthInBytes -Sum).Sum
+
+        $AllItems | ForEach-Object -Process {
+            $SizeInPercent = $_.LengthInBytes / $TotalSize * 100
+            $SizeInPercentRounded = ([math]::round(([math]::round($SizeInPercent) / 10)))
+
+            $_.SizeInPercent = $SizeInPercent
+            $_.SizeInPercentRounded = $SizeInPercentRounded
+            $_.SizeVisualised = ($SizeInPercentRounded..0 | ForEach-Object { '#' }) -join ''
+        }
+
+        # foreach ($Item in $AllItems) {
+        #     $Item.LengthInBytes
+        # }
+
+        $AllItems | Sort-Object LengthInBytes -Descending
+    }
+
 }
--- function/Get-HumanFriendlyFileList/Get-HumanFriendlyFileList.ps1
+++ function/Get-HumanFriendlyFileList/Get-HumanFriendlyFileList.ps1
@@ -3,15 +3,20 @@
     param (
         $Path = $env:USERPROFILE
         [switch]$Recurse = $false
-        [switch]$ExpandSubfoler = $false
+        [switch]$IncludeFolder = $true
+        [switch]$ExpandIntoSubfoler = $false
     )
     
     begin {
-        $InputPath = Get-ChildItem -Path $Path -Recurse $Recurse | Group-Object Parent | Sort-Object Name
+       # $InputPath = Get-ChildItem -Path $Path -Recurse $Recurse | Group-Object Parent | Sort-Object Name
+        $InputPath = Get-ChildItem -Path $Path -Recurse $Recurse
     
     process {
-        
-    }
+        foreach ($Item in $InputPath) {
+        FormatTo-HumanFriendlyOutput -Name $Item.Name -Length $Item.Length
+
+    }}
+
     
     end {
         
