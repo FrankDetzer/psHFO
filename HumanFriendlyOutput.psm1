e1ba3efa09db8214804ed73888980ea936fdaa7c
Frankie D RSVLT <57404682+Frankie-D-RSVLT@users.noreply.github.com>
2020-12-19 23:07:24 +0100

    #

--- HumanFriendlyOutput.psm1
+++ HumanFriendlyOutput.psm1
@@ -1,5 +1,13 @@
-﻿# $global:LangPath = $PSScriptRoot + '\lang'
-# $global:FunctionList = (Get-ChildItem -Path ($PSScriptRoot + '\function') -Recurse -Filter '*.ps1').FullName
+﻿#
+# Module Github Page:   https://github.com/FrankDetzer/HumanFriendlyOutput
+# Module Release Page:  https://frankdetzer.com/release-of-my-ncdu-clone-for-powershell
+# Module Version:       1.00
+# Module Date:          2020-12-20
+#
+# Author Website:       https://frankdetzer.com
+# Author Twitter:       https://twitter.com/frankdetzer
+
+
 
 foreach ($Function in (Get-ChildItem -Path ($PSScriptRoot + '\function') -Recurse -Filter '*.ps1').FullName) {
     . $Function
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# HumanFriendlyOutput
- pwsh HumanFriendlyOutput
+![test2](https://user-images.githubusercontent.com/57404682/102700534-9c2cdc00-424e-11eb-8cd0-7c20cd179e1d.png)
